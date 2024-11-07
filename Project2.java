f62ee02734eb9368fb6eb2685f51850818f96705
alfredscyriac <alfredscyriac@gmail.com>
2024-11-07 13:25:00 -0500

    Read in txt file using TextFileInput and store each type in a seperate sorted linked list

--- Project2.java
+++ Project2.java
@@ -1,12 +1,31 @@
 public class Project2 {
     public static void main(String[] args) {
-        SortedApplianceList applianceList = new SortedApplianceList();
+        SortedApplianceList refrigerators = new SortedApplianceList();
+        SortedApplianceList dishwashers = new SortedApplianceList();
+        SortedApplianceList microwaves = new SortedApplianceList();
+        
         TextFileInput fileInput = new TextFileInput("p2input.txt");
         String line;
         while ((line = fileInput.readLine()) != null) {
             String[] parts = line.split(",");
             String serial = parts[0];
+            Appliance serial2 = parts[0];
             double price = Double.parseDouble(parts[1]);
+            if(serial2.getApplianceType() == 'R'){
+                int cubic_feet = Integer.parseInt(parts[2]);
+                refrigerators.add(new Refrigerator(serial, price, cubic_feet));
+            }
+            else if(serial2.getApplianceType()=='M'){
+                int watts = Integer.parseInt(parts[2]);
+                microwaves.add(new Microwave(serial, price, watts));
+            }
+            else if(serial2.getApplianceType()=="D"){
+                boolean undercounterInstallation = parts[2].equalsIgnoreCase("Y");
+                dishwashers.add(new Dishwasher(serial, price, undercounterInstallation));
+            }
         }
+        fileInput.close();
+        new ApplianceGUI(refrigerators, dishwashers, microwaves);
+
     }
 }
