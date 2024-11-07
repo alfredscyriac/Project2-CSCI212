0ab94d678e9336fcf2123d3f944608d19a49277e
alfredscyriac <alfredscyriac@gmail.com>
2024-11-06 23:12:51 -0500

    Create sorting algorithm for linked list class

--- Project2.java
+++ Project2.java
@@ -1,3 +1,12 @@
 public class Project2 {
-    
+    public static void main(String[] args) {
+        SortedApplianceList applianceList = new SortedApplianceList();
+        TextFileInput fileInput = new TextFileInput("p2input.txt");
+        String line;
+        while ((line = fileInput.readLine()) != null) {
+            String[] parts = line.split(",");
+            String serial = parts[0];
+            double price = Double.parseDouble(parts[1]);
+        }
+    }
 }
--- SortedApplianceList.java
+++ SortedApplianceList.java
@@ -1,3 +1,18 @@
-public class SortedApplianceList {
-    
+public class SortedApplianceList extends ApplianceList{
+    public SortedApplianceList() {
+        super();
+    }
+    public void add(Appliance appliance) {
+        ApplianceNode newNode = new ApplianceNode(appliance);
+        ApplianceNode current = head;
+        while (current.next != null && current.next.data.compareTo(appliance) < 0) {
+            current = current.next;
+        }
+        newNode.next = current.next;
+        current.next = newNode;
+        if (newNode.next == null) {
+            last = newNode;
+        }
+        length++;
+    }
 }
