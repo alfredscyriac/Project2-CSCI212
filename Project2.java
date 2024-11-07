431fdba8c88dd0f1d334cdcf12e0479a8137a521
alfredscyriac <alfredscyriac@gmail.com>
2024-11-07 13:37:04 -0500

    Replace .getAttributeType() with .charAt() b/c as the Appliance class is abstract we are unable to create an Appliance variable in Project2.java. We need an appliance variable for .getApplianceType so replacing it with .charAt is the only solution

--- Project2.java
+++ Project2.java
@@ -9,23 +9,21 @@ public class Project2 {
         while ((line = fileInput.readLine()) != null) {
             String[] parts = line.split(",");
             String serial = parts[0];
-            Appliance serial2 = parts[0];
             double price = Double.parseDouble(parts[1]);
-            if(serial2.getApplianceType() == 'R'){
+            if(parts[0].charAt(0) == 'R'){
                 int cubic_feet = Integer.parseInt(parts[2]);
                 refrigerators.add(new Refrigerator(serial, price, cubic_feet));
             }
-            else if(serial2.getApplianceType()=='M'){
+            else if(parts[0].charAt(0)=='M'){
                 int watts = Integer.parseInt(parts[2]);
                 microwaves.add(new Microwave(serial, price, watts));
             }
-            else if(serial2.getApplianceType()=="D"){
+            else if(parts[0].charAt(0)=='D'){
                 boolean undercounterInstallation = parts[2].equalsIgnoreCase("Y");
                 dishwashers.add(new Dishwasher(serial, price, undercounterInstallation));
             }
         }
         fileInput.close();
         new ApplianceGUI(refrigerators, dishwashers, microwaves);
-
     }
 }
