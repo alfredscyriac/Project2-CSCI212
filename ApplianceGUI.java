5cebba5bb34057636841f2c4dad178f2e25209e0
alfredscyriac <alfredscyriac@gmail.com>
2024-11-07 16:36:23 -0500

    Create first column of Jframe which interates through the sorted appliance list and prints the toString to screen and goes to next line

--- ApplianceGUI.java
+++ ApplianceGUI.java
@@ -6,6 +6,14 @@ public class ApplianceGUI {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
         frame.setLayout(new GridLayout(1, 3)); 
         frame.setTitle("Project 2 - Appliance List w Linked Lists");
+
+        JTextArea fridgeArea = new JTextArea();
+        ApplianceNode current = refrigerators.head;
+        while (current != null){
+            fridgeArea.append(current.data.toString() + "\n");
+            current = current.next;
+        }
+        frame.add(new JScrollPane(fridgeArea));
     }
     
 }
