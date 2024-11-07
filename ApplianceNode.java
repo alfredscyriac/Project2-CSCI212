1cd2add76052be01ca5a330da0d44dede31b28a6
alfredscyriac <alfredscyriac@gmail.com>
2024-11-06 19:18:13 -0500

    Create class for each node that will be used for linked lists

---
+++ ApplianceNode.java
@@ -0,0 +1,8 @@
+public class ApplianceNode {
+    protected Appliance data;
+    protected ApplianceNode next;
+    public ApplianceNode(Appliance data){
+        this.data = data;
+        this.next=null;
+    }
+}
