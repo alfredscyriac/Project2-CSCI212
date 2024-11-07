99306b52408fc1c1378fd465fd219c5990dbb81e
alfredscyriac <alfredscyriac@gmail.com>
2024-11-07 16:32:13 -0500

    Create constructor ApplianceGUI that takes in 3 SortedApplianceList parameters and create JFrame

--- ApplianceGUI.java
+++ ApplianceGUI.java
@@ -1,3 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
 public class ApplianceGUI {
+    public ApplianceGUI(SortedApplianceList refrigerators, SortedApplianceList dishwahsers, SortedApplianceList microwaves){
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+        frame.setLayout(new GridLayout(1, 3)); 
+        frame.setTitle("Project 2 - Appliance List w Linked Lists");
+    }
     
 }
